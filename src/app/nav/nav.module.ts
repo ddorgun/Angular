438bddeeb6afc367a05fbc0152d4ae6b96c2acc9
정철균 <cg.jeong@alticast.com>
2018-07-19 16:13:54 +0900

    toolbar

--- src/app/nav/nav.module.ts
+++ src/app/nav/nav.module.ts
@@ -4,12 +4,25 @@ import { NavComponent } from './nav.component';
 
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import {
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatSidenavModule
+} from '@angular/material';
+
 
 @NgModule({
     imports: [
         CommonModule,
         MatDividerModule,
-        MatListModule
+        MatListModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatButtonToggleModule,
+        MatSidenavModule
     ],
     declarations: [NavComponent],
     exports: [NavComponent]
